59440f616b6e82455d4f32cad89a3fa52f0ce64b
Julián Palomo <julian@usemarkup.com>
2020-04-03 12:59:22 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 
-const Pet = ({ name, animal, breed }) =>
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h1", {}, animal),
-    React.createElement("h1", {}, breed),
-  ]);
+import Pet from "./Pet";
 
-const App = () =>
-  React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+const App = () => (
+  <div>
+    <h1 id="something-important">Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="Cat" breed="Mixed" />
+  </div>
+);
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
