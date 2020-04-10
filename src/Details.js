a145241a13bd9557f50a3a6e53d98e507a8d1b2c
Julián Palomo <julian@usemarkup.com>
2020-04-10 10:48:27 +0100

    Added Details class component

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,39 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => (
-  // to see the data from the API
-  <pre>
-    <code>{JSON.stringify(props, null, 4)}</code>
-  </pre>
-);
+// Function component
+// const Details = (props) => (
+//   // to see the data from the API
+//   <pre>
+//     <code>{JSON.stringify(props, null, 4)}</code>
+//   </pre>
+// );
+
+// Class component
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) =>
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      })
+    );
+  }
+
+  render() {}
+}
 
 export default Details;
